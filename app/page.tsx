acd1132dc3807ff55027ec0d49edf77dda295ca1
Jayesh Shrivastava <jayeshshrivastava815@gmail.com>
2025-09-28 17:44:21 +0530

    Initial Commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+const Home = () => {
+  return (
+    <>
+      <div className=" flex justify-center items-center h-screen">
+        <Button>Hello chai</Button>
+      </div>
+    </>
+  );
+};
+
+export default Home;
